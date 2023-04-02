a0d065416c7d5d03f89c1212ab04cab66b317e7d
Brandon Conway <brandoncc@gmail.com>
2023-04-01 22:48:17 -0700

    BareRepository has its own context

--- src/repository.rs
+++ src/repository.rs
@@ -1,13 +1,22 @@
 use std::path::PathBuf;
 
-use crate::{commands::git_command, utils::is_bare_repo, Context, bare_repo, normal_repo};
+use crate::{
+    bare_repo,
+    commands::git_command,
+    normal_repo,
+    utils::{get_bare_root, get_main_branch_name, is_bare_repo},
+    Context,
+};
 
 #[cfg(test)]
 mod tests;
 
 pub struct Repository;
 
-struct BareRepository {}
+struct BareRepository {
+    main_branch_name: String,
+    root: PathBuf,
+}
 
 struct NormalRepository {}
 
@@ -16,7 +25,7 @@ pub trait RepositoryInterface {
     fn is_bare(self: &Self) -> bool;
 }
 
-impl RepositoryInterface for BareRepository {
+impl<'a> RepositoryInterface for BareRepository {
     fn clean_merged(self: &Self, context: &Context) -> Result<(), String> {
         bare_repo::clean_merged_worktrees(context)
     }
@@ -36,6 +45,23 @@ impl RepositoryInterface for NormalRepository {
     }
 }
 
+impl BareRepository {
+    pub fn at(path: &PathBuf) -> Option<Self> {
+        if !path.exists() {
+            return None;
+        }
+
+        if !is_repo(path) {
+            return None;
+        }
+
+        Some(Self {
+            main_branch_name: get_main_branch_name(path),
+            root: get_bare_root(path).expect("Expected to find a bare repo root, but didn't"),
+        })
+    }
+}
+
 impl Repository {
     pub fn at(path: &PathBuf) -> Option<Box<dyn RepositoryInterface>> {
         if !path.exists() {
@@ -47,7 +73,9 @@ impl Repository {
         }
 
         match is_bare_repo(path) {
-            true => Some(Box::new(BareRepository {})),
+            true => Some(Box::new(BareRepository::at(path).expect(
+                format!("{:#?} is not a valid git repository", path).as_str(),
+            ))),
             false => Some(Box::new(NormalRepository {})),
         }
     }
--- src/repository/tests.rs
+++ src/repository/tests.rs
@@ -4,6 +4,7 @@ use std::env::temp_dir;
 #[cfg(test)]
 use std::path::PathBuf;
 
+use crate::repository::BareRepository;
 #[cfg(test)]
 use crate::test_helpers::run_test;
 
@@ -20,9 +21,9 @@ use crate::test_setup::CLEAN_NORMAL_REPO_NAME;
 use super::Repository;
 
 #[test]
-fn test_at_returns_a_bare_repository_for_the_bare_root_path() {
+fn test_repository_at_returns_a_bare_repository_for_the_bare_root_path() {
     run_test(
-        "test_at_returns_a_bare_repository_for_the_bare_root_path",
+        "test_repository_at_returns_a_bare_repository_for_the_bare_root_path",
         BARE_REPO_NAME,
         &RepoType::Bare,
         |context: Context| match Repository::at(&context.repo_path) {
@@ -33,9 +34,9 @@ fn test_at_returns_a_bare_repository_for_the_bare_root_path() {
 }
 
 #[test]
-fn test_at_returns_a_bare_repository_for_a_valid_repo_subdirectory_path() {
+fn test_repository_at_returns_a_bare_repository_for_a_valid_repo_subdirectory_path() {
     run_test(
-        "test_at_returns_a_bare_repository_for_a_valid_repo_subdirectory_path",
+        "test_repository_at_returns_a_bare_repository_for_a_valid_repo_subdirectory_path",
         BARE_REPO_NAME,
         &RepoType::Bare,
         |context: Context| match Repository::at(&context.repo_path.join("merged")) {
@@ -46,9 +47,9 @@ fn test_at_returns_a_bare_repository_for_a_valid_repo_subdirectory_path() {
 }
 
 #[test]
-fn test_at_returns_a_normal_repository() {
+fn test_repository_at_returns_a_normal_repository() {
     run_test(
-        "test_at_returns_a_normal_repository",
+        "test_repository_at_returns_a_normal_repository",
         CLEAN_NORMAL_REPO_NAME,
         &RepoType::Normal,
         |context: Context| match Repository::at(&context.repo_path) {
@@ -59,7 +60,7 @@ fn test_at_returns_a_normal_repository() {
 }
 
 #[test]
-fn test_at_returns_none_for_an_invalid_path() {
+fn test_repository_at_returns_none_for_an_invalid_path() {
     let path = PathBuf::from("/tmp/invalid-repo-path");
     let repo = Repository::at(&path);
 
@@ -67,10 +68,91 @@ fn test_at_returns_none_for_an_invalid_path() {
 }
 
 #[test]
-fn test_at_returns_none_for_a_non_repo_path() {
+fn test_repository_at_returns_none_for_a_non_repo_path() {
     // use /tmp or equivalent because it is guaranteed to exist and also will not be a repo path
     let path = PathBuf::from(temp_dir());
     let repo = Repository::at(&path);
+
+    assert!(repo.is_none());
+}
+
+#[test]
+fn test_bare_repository_at_with_subdirectory_has_correct_root() {
+    run_test(
+        "test_bare_repository_at_with_subdirectory_has_correct_root",
+        BARE_REPO_NAME,
+        &RepoType::Bare,
+        |context: Context| {
+            let path = context.repo_path.join("merged");
+            let repo = BareRepository::at(&path)
+                .expect(format!("{:#?} is not a valid git repository", &path).as_str());
+
+            assert_eq!(PathBuf::from(context.repo_path), repo.root);
+        },
+    );
+}
+
+#[test]
+fn test_bare_repository_at_with_root_has_correct_root() {
+    run_test(
+        "test_bare_repository_at_with_root_has_correct_root",
+        BARE_REPO_NAME,
+        &RepoType::Bare,
+        |context: Context| {
+            let repo = BareRepository::at(&context.repo_path).expect(
+                format!("{:#?} is not a valid git repository", &context.repo_path).as_str(),
+            );
+
+            assert_eq!(PathBuf::from(context.repo_path), repo.root);
+        },
+    );
+}
+
+#[test]
+fn test_bare_repository_at_with_subdirectory_has_correct_main_branch_name() {
+    run_test(
+        "test_bare_repository_at_with_subdirectory_has_correct_main_branch_name",
+        BARE_REPO_NAME,
+        &RepoType::Bare,
+        |context: Context| {
+            let path = context.repo_path.join("merged");
+            let repo = BareRepository::at(&path)
+                .expect(format!("{:#?} is not a valid git repository", &path).as_str());
+
+            assert_eq!("main", repo.main_branch_name);
+        },
+    );
+}
+
+#[test]
+fn test_bare_repository_at_with_root_has_correct_main_branch_name() {
+    run_test(
+        "test_bare_repository_at_with_root_has_correct_main_branch_name",
+        BARE_REPO_NAME,
+        &RepoType::Bare,
+        |context: Context| {
+            let repo = BareRepository::at(&context.repo_path).expect(
+                format!("{:#?} is not a valid git repository", &context.repo_path).as_str(),
+            );
+
+            assert_eq!("main", repo.main_branch_name);
+        },
+    );
+}
+
+#[test]
+fn test_bare_repository_at_returns_none_for_an_invalid_path() {
+    let path = PathBuf::from("/tmp/invalid-repo-path");
+    let repo = BareRepository::at(&path);
+
+    assert!(repo.is_none());
+}
+
+#[test]
+fn test_bare_repository_at_returns_none_for_a_non_repo_path() {
+    // use /tmp or equivalent because it is guaranteed to exist and also will not be a repo path
+    let path = PathBuf::from(temp_dir());
+    let repo = BareRepository::at(&path);
 
     assert!(repo.is_none());
 }
